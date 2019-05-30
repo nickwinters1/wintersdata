672e3e30e8995cd2e2815ed844df0c1daf7b8762
Nick Winters <nicklaus.winters@gmail.com>
2019-05-30 17:17:41 -0500

    __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+Biovir3 Helper DS Functions
+"""
+import pandas as pd
+import numpy as np
+
+from . import example_module
+from . import functions.Cleaner
+
+TEST = pd.DataFrame(np.ones(10))
